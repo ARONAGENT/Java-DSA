baa6c05fd8104869e573b7127fccbb527832b59e
Rohan Uke <ukerohan7@gmail.com>
2025-04-16 01:51:27 +0530

    add New Topic OOPs->Inheritance -> Single Inheritance

--- src/oops/inheritance/mainClass.java
+++ src/oops/inheritance/mainClass.java
@@ -4,6 +4,7 @@ public class mainClass {
     public static void main(String[] args) {
         Two two=new Two();
         int val=two.sum(2,3);
+        // here Two obj called One Class method called Single inheritance
         int val2 =two.calSquare(6);
         System.out.println("Sum is "+val);
         System.out.println("Square is "+val2);
