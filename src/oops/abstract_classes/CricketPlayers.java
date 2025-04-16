329441704347833ec2acb1a07e54aea6b5284a3e
Rohan Uke <ukerohan7@gmail.com>
2025-04-16 18:48:47 +0530

    Learn New Topic OOPs-> Abstract Classes

---
+++ src/oops/abstract_classes/CricketPlayers.java
@@ -0,0 +1,11 @@
+package oops.abstract_classes;
+
+public class CricketPlayers {
+    public static void main(String[] args) {
+        //'Players' is abstract; cannot be instantiated
+        // By means We cannot create object or instance of abstract class
+        // here abstract class is players  hence it can't allow to create the object of the class
+        // Players players=new Players() ;
+    }
+
+}
---
+++ src/oops/abstract_classes/Players.java
@@ -0,0 +1,8 @@
+package oops.abstract_classes;
+
+public abstract class Players {
+
+    public void hello(){
+        System.out.println("Hello i am Rohan And I am a cricketer");
+    }
+}
