19103e397ae1ad581036721810822e04c6f73b76
Rohan Uke <ukerohan7@gmail.com>
2025-06-21 21:44:25 +0530

    Learn how to reverse the array

---
+++ src/arrays/focus_dsa/ReverseArray.java
@@ -0,0 +1,10 @@
+package arrays.focus_dsa;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr={2,4,34,56,76,43,21,22,445};
+        for(int i= arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
