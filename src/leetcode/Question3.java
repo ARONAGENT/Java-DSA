c3de36d0b1308f0b4806aa6062be5f69e53f3560
Rohan Uke <ukerohan7@gmail.com>
2025-06-23 01:16:35 +0530

    update question 3

--- src/leetcode/Question3.java
+++ src/leetcode/Question3.java
@@ -1,13 +1,21 @@
 package leetcode;
 
+import java.awt.desktop.PreferencesEvent;
+
 public class Question3 {
     public boolean isPowerOf2(int x){
-        return x % 10 == 1 || x % 10 == 4 || x % 10 == 6 || x % 10 == 8;
+        if(x<1){
+            return false;
+        }
+        while(x%2==0){
+            x/=2;
+        }
+        return x==1;
     }
 
     public static void main(String[] args) {
         Question3 question3=new Question3();
-        boolean val=question3.isPowerOf2(128);
+        boolean val=question3.isPowerOf2(34);
         System.out.println(val);
     }
 }
